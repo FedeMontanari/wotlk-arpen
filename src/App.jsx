9c3602d2e57bfe8fa56a1232c0f9a017a5041f5f
Federico Montanari <fedemontanari@outlook.com>
2023-03-01 21:55:30 -0300

    Removed button; Style changes; Added credits

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import "./App.css";
 
 export default function App() {
   const [input, setInput] = useState({
-    arpen: 0,
+    arpen: "",
     debuffs: {
       sunder: false,
       faerie: false,
@@ -24,58 +24,61 @@ export default function App() {
     }
     setInput({
       ...input,
-      [e.target.name]: Number(e.target.value),
+      [e.target.name]: e.target.value,
     });
   }
 
-  //Issue where data doesnt update if you ONLY change arpen rating input
-
-  const [loading, setLoading] = useState(true);
-
   const bossArmor = 10643;
-
   const [armorModifier, setArmorModifier] = useState(0);
-
   const [armor, setArmor] = useState(0);
+  const [arpenCap, setArpenCap] = useState(0);
+  const [effectiveArmor, setEffectiveArmor] = useState(0);
+  const [damageReduced, setDamageReduced] = useState(0);
+  const [damageApplied, setDamageApplied] = useState(0);
 
   useEffect(() => {
     setArmor(bossArmor * armorModifier);
   }, [armorModifier]);
 
-  const [arpenCap, setArpenCap] = useState(0);
+  useEffect(() => {
+    let counter = 1;
+    if (input.debuffs.sunder) {
+      counter *= 1 - 0.2;
+    }
+    if (input.debuffs.faerie) {
+      counter *= 1 - 0.05;
+    }
+    if (input.debuffs.shattering) {
+      counter *= 1 - 0.2;
+    }
+    setArmorModifier(counter);
+  }, [input]);
 
   useEffect(() => {
     setArpenCap((armor + 15232.5) / 3);
   }, [armor]);
 
-  const [effectiveArmor, setEffectiveArmor] = useState(0);
-
   useEffect(() => {
     if (arpenCap < armor) {
-      setEffectiveArmor(armor - (input.arpen / 1399.6) * arpenCap);
+      setEffectiveArmor(armor - (Number(input.arpen) / 1399.6) * arpenCap);
     } else {
-      setEffectiveArmor(armor - (input.arpen / 1399.6) * armor);
+      setEffectiveArmor(armor - (Number(input.arpen) / 1399.6) * armor);
     }
   }, [arpenCap, input.arpen]);
-
-  const [damageReduced, setDamageReduced] = useState(0);
 
   useEffect(() => {
     setDamageReduced(effectiveArmor / (effectiveArmor + 15232.5));
   }, [effectiveArmor]);
 
-  const [damageApplied, setDamageApplied] = useState(0);
-
   useEffect(() => {
     setDamageApplied(1 - damageReduced);
-    setLoading(false);
   }, [damageReduced]);
 
   useEffect(() => {
     const resultDiv = document.getElementById("results");
-    if (!loading) {
-      resultDiv.hidden = false;
-      resultDiv.innerHTML = `
+
+    resultDiv.hidden = false;
+    resultDiv.innerHTML = `
         <span>
         Boss Armor: <b>${bossArmor}</b>
       </span>
@@ -98,87 +101,93 @@ export default function App() {
         2
       )} %</b></span>
         `;
-      return;
-    }
-  }, [loading]);
-
-  function handleOnClick(e) {
-    e.preventDefault();
-    setLoading(true);
-    let counter = 1;
-    if (input.debuffs.sunder) {
-      counter *= 1 - 0.2;
-    }
-    if (input.debuffs.faerie) {
-      counter *= 1 - 0.05;
-    }
-    if (input.debuffs.shattering) {
-      counter *= 1 - 0.2;
-    }
-    setArmorModifier(counter);
-  }
+    return;
+  }, [damageApplied]);
 
   return (
     <div id="App">
       <h1>WoW WOTLK ArPen Rating Calculator</h1>
-      <div id="form">
-        <form>
-          <label htmlFor="arpen">Your ArPen Rating here:</label>
-          <input
-            type="number"
-            name="arpen"
-            id="arpen"
-            value={input.arpen}
-            onChange={handleInputChange}
-          />
+      <div className="wrapper">
+        <div id="form">
+          <form>
+            <label htmlFor="arpen">Your ArPen Rating here:</label>
+            <input
+              type="number"
+              inputMode="numeric"
+              pattern="[0-9]+"
+              name="arpen"
+              id="arpen"
+              min={null}
+              value={input.arpen}
+              onChange={handleInputChange}
+            />
 
-          <div
-            style={{
-              display: "flex",
-              justifyContent: "center",
-              alignItems: "center",
-            }}
-          >
-            <fieldset style={{ width: "fit-content" }}>
-              <legend>Debuffs</legend>
-              <input
-                type="checkbox"
-                name="debuffs"
-                id="sunder"
-                value="sunder"
-                onChange={handleInputChange}
-              />
-              <label htmlFor="sunder">Sunder Armor</label>
-              <input
-                type="checkbox"
-                name="debuffs"
-                id="faerie"
-                value="faerie"
-                onChange={handleInputChange}
-              />
-              <label htmlFor="faerie">Faerie Fire</label>
-              <input
-                type="checkbox"
-                name="debuffs"
-                id="shattering"
-                value="shattering"
-                onChange={handleInputChange}
-              />
-              <label htmlFor="shattering">Shattering Throw?</label>
-            </fieldset>
-          </div>
-          <button onClick={(e) => handleOnClick(e)}>Calculate!</button>
-        </form>
+            <div
+              style={{
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+              }}
+            >
+              <fieldset style={{ width: "fit-content" }}>
+                <legend>Debuffs</legend>
+                <input
+                  type="checkbox"
+                  name="debuffs"
+                  id="sunder"
+                  value="sunder"
+                  onChange={handleInputChange}
+                />
+                <label htmlFor="sunder">Sunder Armor</label>
+                <input
+                  type="checkbox"
+                  name="debuffs"
+                  id="faerie"
+                  value="faerie"
+                  onChange={handleInputChange}
+                />
+                <label htmlFor="faerie">Faerie Fire</label>
+                <input
+                  type="checkbox"
+                  name="debuffs"
+                  id="shattering"
+                  value="shattering"
+                  onChange={handleInputChange}
+                />
+                <label htmlFor="shattering">Shattering Throw</label>
+              </fieldset>
+            </div>
+          </form>
+        </div>
+        <div id="results" hidden></div>
       </div>
-      <div id="results" hidden></div>
+      <div id="reference">
+        <p>
+          All the formulas were taken from{" "}
+          <cite>
+            <a href="https://forum.warmane.com/showthread.php?t=298787">this</a>
+          </cite>{" "}
+          Warmane forum article. <br /> All credits to the original poster.
+        </p>
+      </div>
       <div className="madeBy">
         <p>
           Made by{" "}
-          <img
-            src="https://pugfinder.gg/images/icons/classes/warrior.png"
-            alt="Warrior class logo"
-          />{" "}
-          Yakisieras - Frostmourne
+          <a
+            href="https://armory.warmane.com/character/Yakisieras/Frostmourne/summary"
+            style={{
+              textDecoration: "none",
+              color: "rgba(255, 255, 255, 0.87)",
+              display: "flex",
+            }}
+          >
+            <img
+              src="https://pugfinder.gg/images/icons/classes/warrior.png"
+              alt="Warrior class logo"
+            />
+            Yakisieras
+          </a>
+          - Frostmourne
         </p>
       </div>
     </div>
